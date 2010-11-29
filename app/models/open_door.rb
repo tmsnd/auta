65742566afbcdc84387f9dff80443ef4f1b4b047
sherold <sherold@cathead.(none)>
2010-11-29 01:44:09 +0100

    autav20

--- app/models/open_door.rb
+++ app/models/open_door.rb
@@ -6,7 +6,7 @@ class OpenDoor < ActiveRecord::Base
   
   named_scope :box_days, lambda { {:conditions =>["date_from >= ?", Date.today.beginning_of_day], :order => 'date_from', :limit => 5 }}
   named_scope :box_days1, :limit => 1, :order => 'RAND()'
-  named_scope :box_days2, lambda { {:conditions =>["date_from >= ?", Date.today.beginning_of_day], :order => 'date_from, RAND()', :limit => 1 }}
+  named_scope :box_days2, lambda { {:conditions =>["date_from >= ?", Date.today.beginning_of_day], :order => 'date_from, RAND()', :limit => 10 }}
     
   define_index do
     indexes description
